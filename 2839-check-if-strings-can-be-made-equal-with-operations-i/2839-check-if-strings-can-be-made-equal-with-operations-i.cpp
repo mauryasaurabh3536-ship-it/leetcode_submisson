50a637b3580ae1e49a8567aad2c2db4927301fd9
mauryasaurabh3536-ship-it <mauryasaurabh3536@gmail.com>
2026-03-29 20:24:06 +0530

    Time: 4 ms (5.16%), Space: 9 MB (12.9%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
        vector<string>v;
+       string t=s1;
        v.push_back(s1);
        swap(s1[0],s1[2]);
        v.push_back(s1);
+       swap(t[1],t[3]);
+       v.push_back(t);
        swap(s1[1],s1[3]);
        v.push_back(s1);
        for(int i=0;i<v.size();i++){
-        if(s1==s2) return true;
+        if(v[i]==s2) return true;
        }
        return false;
     }
