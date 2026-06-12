a74886969ba302a4e29cf190a26abb16efd8738f
mauryasaurabh3536-ship-it <mauryasaurabh3536@gmail.com>
2026-06-12 09:40:48 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.09%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,16 +2,22 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int time=0;
-        queue<int>q;
-        for(int i=0;i<tickets.size();i++){
-            q.push(i);
-        }
+        // queue<int>q;
+        // for(int i=0;i<tickets.size();i++){
+        //     q.push(i);
+        // }
 
-        while(tickets[k]!=0){
-            tickets[q.front()]--;
-            if(tickets[q.front()]!=0) q.push(q.front());
-            q.pop();
-            time++;
+        // while(tickets[k]!=0){
+        //     tickets[q.front()]--;
+        //     if(tickets[q.front()]!=0) q.push(q.front());
+        //     q.pop();
+        //     time++;
+        // }
+        for(int i=0;i<=k;i++){
+            time+=min(tickets[k],tickets[i]);
+        }
+        for(int i=k+1;i<tickets.size();i++){
+            time+=min(tickets[k]-1,tickets[i]);
         }
         return time;
     }
